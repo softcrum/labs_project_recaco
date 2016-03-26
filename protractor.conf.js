0c076f06a1996f29356e4573dc2e63187a903e40
daniboysc <daniel.valenzuela@softcrum.company>
2016-03-25 23:31:28 -0300

    Fix protractor.conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,75 +1,36 @@
-// Protractor configuration
-// https://github.com/angular/protractor/blob/master/referenceConf.js
-
 'use strict';
 
-var config = {
-  // The timeout for each script run on the browser. This should be longer
-  // than the maximum time your application needs to stabilize between tasks.
+const config = {
   allScriptsTimeout: 110000,
-
-  // A base URL for your application under test. Calls to protractor.get()
-  // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:' + (process.env.PORT || '9000'),
-
-  // Credientials for Saucelabs
-  sauceUser: process.env.SAUCE_USERNAME,
-
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-
-  // list of files / patterns to load in the browser
-  specs: [
-    'e2e/**/*.spec.js'
-  ],
-
-  // Patterns to exclude.
-  exclude: [],
-
-  // ----- Capabilities to be passed to the webdriver instance ----
-  //
-  // For a full list of available capabilities, see
-  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
-  // and
-  // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
     'browserName': 'chrome',
+    'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Fullstack E2E',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },
-
-  // ----- The test framework -----
-  //
-  // Jasmine and Cucumber are fully supported as a test and assertion framework.
-  // Mocha has limited beta support. You will need to include your own
-  // assertion framework if working with mocha.
+  exclude: [],
   framework: 'jasmine2',
-
-  // ----- Options to be passed to minijasminenode -----
-  //
-  // See the full list at https://github.com/jasmine/jasmine-npm
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000,
-    print: function() {}  // for jasmine-spec-reporter
+    print: function() {}
   },
-
-  // Prepare environment for tests
+  onPrepare: function() {
+    require('babel-register');
+    const mongoose = require('mongoose');
+    const SpecReporter = require('jasmine-spec-reporter');
+    const serverConfig = config.params.serverConfig;
+    jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: true }));
+    mongoose.connect(serverConfig.mongo.uri, serverConfig.mongo.options);
+  },
   params: {
     serverConfig: require('./server/config/environment')
   },
-
-  onPrepare: function() {
-    require('babel-register');
-    var SpecReporter = require('jasmine-spec-reporter');
-    // add jasmine spec reporter
-    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
-
-    var serverConfig = config.params.serverConfig;
-
-    // Setup mongo for tests
-    var mongoose = require('mongoose');
-    mongoose.connect(serverConfig.mongo.uri, serverConfig.mongo.options); // Connect to database
-  }
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  sauceUser: process.env.SAUCE_USERNAME,
+  specs: [
+    'e2e/**/*.spec.js'
+  ]
 };
 
 config.params.baseUrl = config.baseUrl;
